b5eb3faaaadcb015bca001481fffe8e4538b29fc
Sergey Ternovtsov <tob1az@ukr.net>
2022-12-04 09:20:25 +0200

    Add solution for Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,7 @@
 mod data;
 
-fn calculate_solution(calories_list: &str) -> usize {
-    calories_list
+fn calculate_solution(calories_list: &str) -> (usize, usize) {
+    let mut calories_per_elf = calories_list
         .split("\n\n")
         .map(|calories| {
             calories
@@ -9,8 +9,11 @@ fn calculate_solution(calories_list: &str) -> usize {
                 .map(|food| food.parse::<usize>().unwrap_or_default())
                 .sum::<usize>()
         })
-        .max()
-        .unwrap_or_default()
+        .collect::<Vec<_>>();
+    calories_per_elf.sort_unstable();
+    let most_nutritious = *calories_per_elf.iter().last().unwrap();
+    let top_3 = calories_per_elf.iter().rev().take(3).sum();
+    (most_nutritious, top_3)
 }
 
 fn main() {
