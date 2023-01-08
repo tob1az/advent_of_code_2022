a634944c721c08152a66bef6c7d511a354ad8742
Sergey Ternovtsov <tob1az@ukr.net>
2023-01-09 00:36:01 +0200

    Add solution for Day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -6,6 +6,29 @@ type TreeHeight = i8;
 type TreeVisibility = (TreeHeight, bool);
 type VisibleTrees = Array2<TreeVisibility>;
 
+struct TreeHeightIter<'a> {
+    trees: &'a VisibleTrees,
+    x_step: i32,
+    y_step: i32,
+    x: i32,
+    y: i32,
+}
+
+impl<'a> Iterator for TreeHeightIter<'a> {
+    type Item = &'a TreeHeight;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (x, y) = (self.x + self.x_step, self.y + self.y_step);
+        if x < 0 || y < 0 {
+            None
+        } else {
+            self.x = x;
+            self.y = y;
+            self.trees.get((x as usize, y as usize)).map(|(h, _)| h)
+        }
+    }
+}
+
 fn update_tree_visibility(
     max_height: &mut TreeHeight,
     visibility: &TreeVisibility,
@@ -59,10 +82,52 @@ fn parse_trees(tree_heights: &str) -> VisibleTrees {
     .expect("Correct rectangle of tree heights")
 }
 
-fn calculate_solution(tree_heights: &str) -> usize {
+fn find_highest_scenic_score(trees: VisibleTrees) -> usize {
+    trees
+        .indexed_iter()
+        .map(|((x, y), &(h, _))| calculate_scenic_score(&trees, x, y, h))
+        .max()
+        .expect("at least one tree")
+}
+
+fn calculate_scenic_score(
+    trees: &VisibleTrees,
+    column: usize,
+    row: usize,
+    height: TreeHeight,
+) -> usize {
+    if column == 0 || column == trees.ncols() - 1 || row == 0 || row == trees.nrows() - 1 {
+        return 0;
+    }
+    [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        .into_iter()
+        .map(|(dx, dy)| {
+            TreeHeightIter {
+                trees,
+                x: column as i32,
+                y: row as i32,
+                x_step: dx,
+                y_step: dy,
+            }
+            .scan(false, |found_occlusion, &h| {
+                if *found_occlusion {
+                    None
+                } else {
+                    *found_occlusion = h >= height;
+                    Some(h)
+                }
+            })
+            .count()
+        })
+        .product()
+}
+
+fn calculate_solution(tree_heights: &str) -> (usize, usize) {
     let visible_trees: VisibleTrees =
         mark_visible_tree_row(mark_visible_tree_row(parse_trees(tree_heights).view()).t());
-    visible_trees.into_iter().filter(|(_, v)| *v).count()
+    let observable_tree_count = visible_trees.iter().filter(|(_, v)| *v).count();
+    let highest_scenic_score = find_highest_scenic_score(visible_trees);
+    (observable_tree_count, highest_scenic_score)
 }
 
 fn main() {
