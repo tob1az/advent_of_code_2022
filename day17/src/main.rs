ac0e2ae83dcd1bd1cc95e9b83e5c904a1e2b4062
Serhii Ternovtsov <tserg42@gmail.com>
2023-10-05 23:20:37 +0300

    D17P2: Tetris with cycle detection

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,7 +1,7 @@
 mod data;
 mod tetris;
 
-fn parse_moves(jet_pattern: &str) -> Vec<i32> {
+fn parse_moves(jet_pattern: &str) -> Vec<i64> {
     jet_pattern
         .chars()
         .map(|c| match c {
@@ -12,13 +12,40 @@ fn parse_moves(jet_pattern: &str) -> Vec<i32> {
         .collect()
 }
 
-fn calculate_solution(jet_pattern: &str) -> usize {
-    let moves = parse_moves(jet_pattern);
+fn compute_rock_tower_height(moves: &[i64], total_throws: usize) -> usize {
     let mut sim = tetris::Simulation::new(7, 2, &moves);
-    for _ in 0..2022 {
+    let mut skipped_throws = 0;
+    let mut skipped_cycles = 0;
+    let mut cycle_height = 0;
+    let mut cycle_found = false;
+    for shapes_thrown in 1..=total_throws {
         sim.throw_next_shape();
+        if shapes_thrown % 1000 == 0 {
+            dbg!(shapes_thrown);
+        }
+        if !cycle_found {
+            if let Some(cycle) = sim.find_throw_cycle() {
+                // fast forward
+                cycle_height = (sim.height() - cycle.height) as usize;
+                let throws_per_cycle = shapes_thrown - cycle.throw_count;
+                skipped_cycles = (total_throws - shapes_thrown) / throws_per_cycle;
+                skipped_throws = skipped_cycles * throws_per_cycle;
+                cycle_found = true;
+            }
+        }
+        if skipped_throws + shapes_thrown == total_throws {
+            break;
+        }
     }
-    sim.height() as usize
+    sim.height() as usize + skipped_cycles * cycle_height
+}
+
+fn calculate_solution(jet_pattern: &str) -> (usize, usize) {
+    let moves = parse_moves(jet_pattern);
+    (
+        compute_rock_tower_height(&moves, 2022),
+        compute_rock_tower_height(&moves, 1_000_000_000_000),
+    )
 }
 
 fn main() {
--- day17/src/tetris.rs
+++ day17/src/tetris.rs
@@ -1,14 +1,16 @@
+use std::collections::HashMap;
+
 // origin is at bottom left corner
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Coordinates {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 #[derive(Clone)]
 pub struct Move {
-    pub dx: i32,
-    pub dy: i32,
+    pub dx: i64,
+    pub dy: i64,
 }
 
 #[derive(Clone, Debug)]
@@ -17,7 +19,7 @@ struct Shape {
 }
 
 impl Coordinates {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
 }
@@ -29,7 +31,7 @@ impl Shape {
             blocks: blocks.into(),
         }
     }
-    fn replicate(&self, origin: &Coordinates) -> Self {
+    fn replicate_at(&self, origin: &Coordinates) -> Self {
         let mut replica = self.clone();
         for block in replica.blocks.iter_mut() {
             block.x += origin.x;
@@ -37,7 +39,12 @@ impl Shape {
         }
         replica
     }
-    fn can_move(&self, shape_move: &Move, chamber_width: i32, other_blocks: &[Coordinates]) -> bool {
+    fn can_move(
+        &self,
+        shape_move: &Move,
+        chamber_width: i64,
+        other_blocks: &[Coordinates],
+    ) -> bool {
         for block in &self.blocks {
             let x = block.x + shape_move.dx;
             let y = block.y + shape_move.dy;
@@ -56,25 +63,36 @@ impl Shape {
     }
 }
 
+#[derive(PartialEq, Eq, Hash, Debug)]
+struct ThrowParameters {
+    relative_heights: Vec<i64>,
+    shape_index: usize,
+    shift_index: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct ThrowResult {
+    pub height: i64,
+    pub throw_count: usize,
+}
+
 #[derive(Debug)]
 pub struct Simulation {
-    width: i32,
-    initial_x: i32,
+    width: i64,
+    initial_x: i64,
     chamber: Vec<Coordinates>,
     shape_sequence: Vec<Shape>,
     shape_index: usize,
-    move_sequence: Vec<i32>,
+    move_sequence: Vec<i64>,
     move_index: usize,
-    height: i32, 
+    height: i64,
+    shapes_thrown: usize,
+    throws: HashMap<ThrowParameters, ThrowResult>,
 }
 
 impl Simulation {
-    pub fn new(width: i32, initial_x: i32, move_sequence: &[i32]) -> Self {
-        assert!(
-            width > 0
-                && initial_x > 0
-                && initial_x < width
-        );
+    pub fn new(width: i64, initial_x: i64, move_sequence: &[i64]) -> Self {
+        assert!(width > 0 && initial_x > 0 && initial_x < width);
         Self {
             width,
             initial_x,
@@ -84,6 +102,8 @@ impl Simulation {
             move_sequence: move_sequence.to_owned(),
             move_index: 0,
             height: 0, // floor
+            shapes_thrown: 0,
+            throws: HashMap::new(),
         }
     }
 
@@ -103,12 +123,16 @@ impl Simulation {
         }
         self.chamber.append(&mut shape.blocks);
         self.height = self.chamber.iter().max_by_key(|c| c.y).unwrap().y + 1;
+        self.shapes_thrown += 1;
     }
-    pub fn height(&self) -> i32 {
+    pub fn height(&self) -> i64 {
         self.height
     }
     fn pick_next_shape(&mut self) -> Shape {
-        let shape = self.shape_sequence[self.shape_index].replicate(&Coordinates { x: self.initial_x, y: self.height + 3 });
+        let shape = self.shape_sequence[self.shape_index].replicate_at(&Coordinates {
+            x: self.initial_x,
+            y: self.height + 3,
+        });
         self.shape_index = (self.shape_index + 1) % self.shape_sequence.len();
         shape
     }
@@ -116,6 +140,38 @@ impl Simulation {
         let dx = self.move_sequence[self.move_index];
         self.move_index = (self.move_index + 1) % self.move_sequence.len();
         Move { dx, dy: 0 }
+    }
+    pub fn find_throw_cycle(&mut self) -> Option<ThrowResult> {
+        let mut heights = (0..self.width)
+            .map(|x| {
+                self.chamber
+                    .iter()
+                    .filter(|c| c.x == x)
+                    .map(|c| c.y + 1)
+                    .max()
+                    .unwrap_or(0)
+            })
+            .collect::<Vec<_>>();
+        let min_y = heights.iter().min().copied().unwrap();
+        for h in heights.iter_mut() {
+            *h -= min_y;
+        }
+        let key = ThrowParameters {
+            relative_heights: heights,
+            shape_index: self.shape_index,
+            shift_index: self.move_index,
+        };
+        let result = self.throws.get(&key).cloned();
+        if result.is_none() {
+            self.throws.insert(
+                key,
+                ThrowResult {
+                    height: self.height,
+                    throw_count: self.shapes_thrown,
+                },
+            );
+        }
+        result
     }
 }
 
