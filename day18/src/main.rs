47420444432c13dd419de7dda554df9f9e2e3e6c
Serhii Ternovtsov <tserg42@gmail.com>
2023-11-17 00:59:27 +0200

    D18P2: Exterior sides count

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 mod data;
 
 type Distance = i64;
@@ -54,7 +56,8 @@ fn parse_cubes(cubes: &str) -> Vec<Cube> {
 
 fn count_surface_sides(cubes: &[Cube]) -> usize {
     let mut surface_sides = Vec::new();
-    for cube in cubes {
+    let cubes: HashSet<&Cube> = HashSet::from_iter(cubes.iter());
+    for cube in &cubes {
         for neighbor in cube.neighbors() {
             if !cubes.contains(&neighbor) {
                 surface_sides.push(neighbor);
@@ -64,9 +67,44 @@ fn count_surface_sides(cubes: &[Cube]) -> usize {
     surface_sides.len()
 }
 
-fn calculate_solution(cubes: &str) -> usize {
+fn point_in_box(point: &Point, min: &Point, max: &Point) -> bool {
+    point.x >= min.x
+        && point.x <= max.x
+        && point.y >= min.y
+        && point.y <= max.y
+        && point.z >= min.z
+        && point.z <= max.z
+}
+
+fn count_exterior_sides(cubes: &[Cube]) -> usize {
+    let cubes: HashSet<&Cube> = HashSet::from_iter(cubes.iter());
+    let mut queue = vec![Cube::new(Point::new(-1, -1, -1))];
+    let mut visited = HashSet::new();
+    // estimated bounds of the volume
+    let min = Point::new(-1, -1, -1);
+    let max = Point::new(25, 25, 25);
+    while let Some(cube) = queue.pop() {
+        for n in cube.neighbors() {
+            if !cubes.contains(&n) && !visited.contains(&n) && point_in_box(&n.origin, &min, &max){
+                queue.push(n);
+            }
+        }
+        visited.insert(cube);
+    }
+    let mut exterior = 0;
+    for cube in &cubes {
+        for n in cube.neighbors() {
+            if visited.contains(&n) {
+                exterior += 1;
+            }
+        }
+    }
+    exterior
+}
+
+fn calculate_solution(cubes: &str) -> (usize, usize) {
     let cubes = parse_cubes(cubes);
-    count_surface_sides(&cubes)
+    (count_surface_sides(&cubes), count_exterior_sides(&cubes))
 }
 
 fn main() {
@@ -83,8 +121,14 @@ mod test {
         assert_eq!(count_surface_sides(&cubes), 10);
     }
     #[test]
-    fn larger_set_case() {
+    fn larger_set() {
         let cubes = parse_cubes(data::LARGER_TEST_SAMPLE);
         assert_eq!(count_surface_sides(&cubes), 64);
+    }
+
+    #[test]
+    fn larger_set_exterior_only() {
+        let cubes = parse_cubes(data::LARGER_TEST_SAMPLE);
+        assert_eq!(count_exterior_sides(&cubes), 58);
     }
 }
